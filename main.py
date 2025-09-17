34cc180e2b0c2bfbde9c4cdfa74781328beccfa1
Carlos <carloscntrs.9902@gmail.com>
2025-09-16 19:36:44 -0500

    Agrego base.html con rutas de navegación y páginas nuevas

--- main.py
+++ main.py
@@ -3,12 +3,24 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return "Hello, World!"
-
-@app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route("/base")
+def base():
+    return render_template('base.html')
+
+@app.route("/mision")
+def mision():
+    return render_template('mision.html')
+
+@app.route("/vision")
+def vision():
+    return render_template('vision.html')
+
+@app.route("/programas")
+def programas():
+    return render_template('programas.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True) 
+    app.run(host='0.0.0.0', port=5000, debug=True)
